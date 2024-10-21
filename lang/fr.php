8544d84a74fad9de71157974798fc081da0dbafc
AlexNesvit <alex.nesvit2000@gmail.com>
2024-10-21 17:24:23 +0200

    demmarage de projet ok

---
+++ lang/fr.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'title' => 'Plateforme de préparation aux entretiens PHP et Laravel',
+    'welcome' => 'Bienvenue sur notre plateforme !',
+    'intro' => 'Ici, vous pouvez vous préparer aux entretiens PHP.',
+    // Ajoutez d'autres traductions ici
+];
